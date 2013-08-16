e8773a539d811680be474cce99cf104ede66ee1a
puppies <puppies@macair.gap.com>
2013-08-16 13:27:06 -0700

    initializing engine

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Engine::Application.config.secret_token = '04fef478a110ad9e88de04a3dd460c76993c2f8be318980e234cd26485a361f53e47187a439b1228f4401b17ba32cb0f08cc9281789d4bcd6a8fedaa83000f77'
